b266efe4ca9346c0ccfa53c5562caf6025085662
Alex <alex@scotttonge.com>
2018-02-09 10:13:59 +0000

    added info notification for view and app files

--- Gemfile
+++ Gemfile
@@ -3,6 +3,7 @@
 source "https://rubygems.org"
 
 gem 'capybara'
+gem 'dotenv'
 gem 'pusher'
 gem 'rake'
 gem 'rspec'
--- app.rb
+++ app.rb
@@ -1,25 +1,39 @@
+require 'sinatra/base'
 require 'pusher'
 require 'sinatra'
 require 'dotenv'
 require 'cgi'
 Dotenv.load('api_keys.env')
 
-pusher = Pusher::Client.new({
-  app_id: ENV['API_ID'],
-  key: ENV['API_KEY'],
-  secret: ENV['API_SECRET'],
-  cluster: ENV['CLUSTER'],
-  encrypted: true
-  })
+class App < Sinatra::Base
 
-get '/' do
-  erb :index
-end
+  pusher = Pusher::Client.new({
+    app_id: ENV['API_ID'],
+    key: ENV['API_KEY'],
+    secret: ENV['API_SECRET'],
+    cluster: ENV['CLUSTER'],
+    encrypted: true
+    })
 
-post '/notification' do
-  message = CGI.escape_html params[:message]
+  get '/' do
+    erb :index
+  end
 
-  pusher.trigger('notifications', 'new_notification', {
-      message: message
-  })
+  post '/notification/success' do
+    message = CGI.escape_html params[:message]
+
+    pusher.trigger('success-notifications', 'success_notification', {
+        message: message
+    })
+  end
+
+  post '/notification/info' do
+    message = CGI.escape_html params[:message]
+
+    pusher.trigger('info-notifications', 'info_notification', {
+        message: message
+    })
+  end
+
+  run! if app_file == $0
 end
