96d6bbbe2d80bfd1ee3da978b09507b7694a05c1
Alex <alex@scotttonge.com>
2018-02-15 16:43:12 +0000

    passing test for posting to notification/success channel

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -6,4 +6,9 @@ describe 'App' do
     get '/'
     expect(last_response.status).to eq 200
   end
+
+  it 'posts to success channel' do
+    post '/notification/success'
+    expect(last_response.status).to eq 200
+  end
 end
